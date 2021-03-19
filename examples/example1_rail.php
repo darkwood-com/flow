b76bcade2c9af1c30030432a82721203b5315d15
Mathieu Ledru <matyo91@gmail.com>
2021-03-19 02:07:58 +0100

    :hammer: refactor example

--- examples/example1_rail.php
+++ examples/example1_rail.php
@@ -13,8 +13,8 @@ use Symfony\Component\Messenger\Stamp\TransportMessageIdStamp;
 $job1 = static function (object $data): \Generator {
     printf("*. #%d : Calculating number %d\n", $data['id'], $data['number']);
 
-    // simulating calculating some "light" operation from 10 to 90 milliseconds
-    $delay = random_int(1, 9) * 10;
+    // simulating calculating some "light" operation from 100 to 900 milliseconds as async generator
+    $delay = random_int(1, 9) * 100;
     yield new Delayed($delay);
     $result = $data['number'];
     $result += $result;
@@ -22,25 +22,19 @@ $job1 = static function (object $data): \Generator {
     printf("*. #%d : Result for number %d is %d and took %d milliseconds\n", $data['id'], $data['number'], $result, $delay);
 
     $data['number'] = $result;
-
-    return $data;
 };
 
 
-$job2 = static function (object $data): \Generator {
+$job2 = static function (object $data): void {
     printf(".* #%d : Calculating number %d\n", $data['id'], $data['number']);
 
-    // simulating calculating some "heavy" operation from 100 to 900 milliseconds
-    $delay = random_int(1, 9) * 100;
-    yield new Delayed($delay);
+    // simulating calculating some "light" operation as anonymous function
     $result = $data['number'];
     $result *= $result;
 
-    printf(".* #%d : Result for number %d is %d and took %d milliseconds\n", $data['id'], $data['number'], $result, $delay);
+    printf(".* #%d : Result for number %d is %d\n", $data['id'], $data['number'], $result);
 
     $data['number'] = $result;
-
-    return $data;
 };
 
 $rails = [
