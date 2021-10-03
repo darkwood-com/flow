8e38f1fa591f6c0718f0f93f2aac4adffe9030cf
Mathieu Ledru <matyo91@gmail.com>
2021-10-03 02:20:44 +0200

    :lipstick: localStorage fix

--- docs/assets/js/darkmode.js
+++ docs/assets/js/darkmode.js
@@ -1,22 +1,9 @@
-const globalDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const localMode = localStorage.getItem('theme');
 
-if (globalDark && (localMode === null)) {
+if (localMode === null) {
 
   localStorage.setItem('theme', 'light');
   document.documentElement.classList.add('data-light-mode');
-
-}
-
-if (globalDark && (localMode === 'dark')) {
-
-  document.documentElement.classList.add('data-dark-mode');
-
-}
-
-if (globalDark && (localMode === 'sepia')) {
-
-  document.documentElement.classList.add('data-sepia-mode');
 
 }
 
